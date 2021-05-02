5d388e1c97c5c67d50e41dd606cdd68e3ee3c11c
Shadrachp <shadrachp@yahoo.com>
2021-05-02 23:20:22 +0800

    Add phonetic equivalent

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
+const phonetic_equivalent = {
+  '0' : 'Zero',
+  '1' : 'One',
+  '2' : 'Two',
+  '3' : 'Three',
+  '4' : 'Four',
+  '5' : 'Five',
+  '6' : 'Six',
+  '7' : 'Seven',
+  '8' : 'Eight',
+  '9' : 'Nine',
+}
+
 //get input from params (args) after node <filename> (console)
 const get_input = () => {
   return process.argv.slice(2);
 };
 
 const main = (input) => {
-  
+  input.map(num => {
+
+  });
 };
 
 main(get_input());
