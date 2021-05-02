51faa41e9a51db80d261abbc5ceba03f156b1b48
Shadrachp <shadrachp@yahoo.com>
2021-05-02 22:07:42 +0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+//get input from params (args) after node <filename> (console)
+const get_input = () => {
+  return process.argv.slice(2);
+};
+
+const main = (input) => {
+  
+};
+
+main(get_input());
